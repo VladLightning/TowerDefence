bc95116d3480e8aa6c7e4b208e7e9941a6a6d5b5
VladLightning <vladlen4102006@gmail.com>
2024-08-08 13:45:29 +0300

    Added tower purchase. Adjusted relevant prefabs.

---
+++ Assets/Scripts/Level/LevelData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "Level")]
+public class LevelData : ScriptableObject
+{
+    [SerializeField] private int _startMoney;
+    public int StartMoney => _startMoney;
+}
