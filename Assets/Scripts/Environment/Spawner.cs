bf62956b06526adf1489e68b0e6e66ff0793ce42
VladLightning <vladlen4102006@gmail.com>
2024-09-22 18:17:42 +0300

    Added start wave button and script.

--- Assets/Scripts/Environment/Spawner.cs
+++ Assets/Scripts/Environment/Spawner.cs
@@ -11,19 +11,37 @@ public class Spawner : MonoBehaviour
     [SerializeField] private int _enemiesAmount;
     [SerializeField] private float _spawnDelay;
 
+    private float _spawnCycleTime;
+    public float SpawnCycleTime => _spawnCycleTime;
+
+    private bool _isSpawning;
+
     private void Start()
     {
-        StartCoroutine(SpawnEnemy());
+        for (int i = 0; i < _enemiesAmount+1; i++)
+        {
+            _spawnCycleTime += _spawnDelay;
+        }
     }
 
     private IEnumerator SpawnEnemy()
     {
+        _isSpawning = true;
         yield return new WaitForSeconds(_spawnDelay);
         for(int i = 0; i < _enemiesAmount; i++)
         {            
             GameObject enemy = Instantiate(_enemyToSpawn, transform.position, transform.rotation);
             enemy.GetComponent<Enemy>().Initiate(_enemyData[0], _movementPath, _playerHealth, _playerMoney);
             yield return new WaitForSeconds(_spawnDelay);
+        }
+        _isSpawning = false;
+    }
+
+    public void StartSpawnEnemy()
+    {
+        if (!_isSpawning)
+        {
+            StartCoroutine(SpawnEnemy());
         }
     }
 }
---
+++ Assets/Scripts/UI/StartWaveButton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartWaveButton : MonoBehaviour
+{
+    [SerializeField] private Spawner[] _spawners;
+
+    private Button _startWaveButton;
+
+    private void Start()
+    {
+        _startWaveButton = GetComponent<Button>();
+    }
+
+    private IEnumerator SetButtonTimer()
+    {
+        float longestSpawnCycle = _spawners[0].SpawnCycleTime;
+        for (int i = 1; i < _spawners.Length; i++)
+        {
+            if (_spawners[i].SpawnCycleTime < longestSpawnCycle)
+            {
+                longestSpawnCycle = _spawners[i].SpawnCycleTime;
+            }
+        }
+
+        yield return new WaitForSeconds(longestSpawnCycle);
+
+        _startWaveButton.interactable = true;
+    }
+
+    public void StartWave()
+    {
+        StartCoroutine(SetButtonTimer());
+
+        _startWaveButton.interactable = false;
+
+        for (int i = 0; i < _spawners.Length; i++)
+        {
+            _spawners[i].StartSpawnEnemy();
+        }
+    }
+}
