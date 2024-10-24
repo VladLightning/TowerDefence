e51bb0a3da3eaf6d3eeb795bfe829ed28fe2a2bf
VladLightning <vladlen4102006@gmail.com>
2024-10-24 16:23:00 +0300

    Refactor

--- Assets/Scripts/UI/TweenAnimation.cs
+++ Assets/Scripts/UI/TweenAnimation.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class TweenAnimation : MonoBehaviour
 {
-    private bool _isPlaying;
-
     public IEnumerator PointAtoB(Transform objectToAnimate, Vector3 target, Ease easeType, float animationStartDelay, float animationDuration)
     {
         yield return new WaitForSeconds(animationStartDelay);
@@ -17,24 +15,11 @@ public class TweenAnimation : MonoBehaviour
 
     public IEnumerator GraphicJump(Transform objectToAnimate, float animationStartDelay, float animationDuration, float jumpHeight)
     {
-        if(_isPlaying)
-        {
-            yield break;
-        }
         yield return new WaitForSeconds(animationStartDelay);
-
-        _isPlaying = true;
 
         objectToAnimate.DOMove(objectToAnimate.transform.position + Vector3.up * jumpHeight, animationDuration);
         yield return new WaitForSeconds(animationDuration);
         objectToAnimate.DOMove(objectToAnimate.transform.position + Vector3.down * jumpHeight, animationDuration);
         yield return new WaitForSeconds(animationDuration);
-
-        _isPlaying = false;
-    }
-
-    private void OnDisable()
-    {
-        transform.DOKill();
     }
 }
