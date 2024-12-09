ccdb2c1dc0ce122cc2a909a6a4b861d41e5ffa8f
VladLightning <vladlen4102006@gmail.com>
2024-12-09 12:30:59 +0200

    Added tornado ability

--- Assets/Scripts/UI/Abilities/TornadoAbility.cs
+++ Assets/Scripts/UI/Abilities/TornadoAbility.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class TornadoAbility : Ability
 {
     [SerializeField] private GameObject _tornadoPrefab;
+
+    private void InitTornado(GameObject tornadoInstance)
+    {
+        var tornado = tornadoInstance.GetComponent<Tornado>();
+        var abilityData = _abilityData as TornadoAbilityData;
+        
+        tornado.SetTornadoStats(abilityData.Mask, abilityData.TornadoForceMagnitude, abilityData.TornadoDuration);
+        tornado.TornadoActivate();
+    }
     
     protected override void AbilityCast()
     {
         base.AbilityCast();
-        Instantiate(_tornadoPrefab, GetMousePosition(), _tornadoPrefab.transform.rotation);
+        var tornadoInstance = Instantiate(_tornadoPrefab, GetMousePosition(), _tornadoPrefab.transform.rotation);
+
+        InitTornado(tornadoInstance);
     }
 }
