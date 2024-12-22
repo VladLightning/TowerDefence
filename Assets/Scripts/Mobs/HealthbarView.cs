5f5d29e5fde9a14cc519eaa07e6322c485c6e419
VladLightning <vladlen4102006@gmail.com>
2024-12-22 16:10:41 +0200

    Refactor

--- Assets/Scripts/Mobs/HealthbarView.cs
+++ Assets/Scripts/Mobs/HealthbarView.cs
@@ -9,7 +9,7 @@ public class HealthbarView : MonoBehaviour
     {
         var healthBar = _healthBar.transform.parent.gameObject;
 
-        healthBar.transform.localScale = new Vector3(transform.parent.localScale.x, transform.parent.localScale.y); //Строка для того, чтобы полоска здоровья не поворачивалась вместе с мобом
+        healthBar.transform.localScale = transform.parent.localScale; //Строка для того, чтобы полоска здоровья не поворачивалась вместе с мобом
         healthBar.SetActive(currentHealth < maxHealth);
 
         _healthBar.fillAmount = (float)currentHealth / maxHealth;
