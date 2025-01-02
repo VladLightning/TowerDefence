7949e5093460cd241c36c94db7e8702a613eeb78
VladLightning <vladlen4102006@gmail.com>
2025-01-02 15:01:42 +0200

    Branch upgrade selection. Refactor.

---
+++ Assets/Scripts/ScriptableObjects/Tower/BranchData/FlameBranch/FlameAbilityOneData.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "FlameAbilityOneData", menuName = "UpgradesData/FlameAbilityOne")]
+public class FlameAbilityOneData : BranchUpgradeData
+{
+    public override string UpgradeClassName => "FlameAbilityOne";
+}
