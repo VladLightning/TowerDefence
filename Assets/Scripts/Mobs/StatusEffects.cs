c5f7167304fcab466901354e33311a5abec5b5bf
VladLightning <vladlen4102006@gmail.com>
2025-01-19 15:18:17 +0200

    Refactor

--- Assets/Scripts/Mobs/StatusEffects.cs
+++ Assets/Scripts/Mobs/StatusEffects.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 public class StatusEffects : MonoBehaviour
 {
     private Mob _mob;
-
-    private StatusProjectileStats _statusProjectileStats;
-    
-    private MobStatusesEnum.MobStatuses _currentStatus;
     
     private void Start()
     {
@@ -16,26 +12,24 @@ public class StatusEffects : MonoBehaviour
 
     public void TriggerEffect(StatusProjectileStats statusProjectileStats, DamageTypesEnum.DamageTypes damageType)
     {
-        _statusProjectileStats = statusProjectileStats;
-
         switch (damageType)
         {
             case DamageTypesEnum.DamageTypes.Flame:
-                _currentStatus = MobStatusesEnum.MobStatuses.Aflame;
-                StartCoroutine(BurningEffect());
+                
+                StartCoroutine(BurningEffect(statusProjectileStats));
                 break;
             case DamageTypesEnum.DamageTypes.Frost:
-                _currentStatus = MobStatusesEnum.MobStatuses.Frozen;
+                
                 break;
         }
     }
 
-    private IEnumerator BurningEffect()
+    private IEnumerator BurningEffect(StatusProjectileStats statusProjectileStats)
     {
-        for (int i = 0; i < _statusProjectileStats.StatusTicksAmount; i++)
+        for (int i = 0; i < statusProjectileStats.StatusTicksAmount; i++)
         {
-            yield return new WaitForSeconds(_statusProjectileStats.StatusTickInterval);
-            _mob.TakeDamage(_statusProjectileStats.StatusDamage);
+            yield return new WaitForSeconds(statusProjectileStats.StatusTickInterval);
+            _mob.TakeDamage(statusProjectileStats.StatusDamage);
         }
     }
 }
