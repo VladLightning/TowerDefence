ee6e7c44aaa99b414b2dc94efb171164bcb99c7f
VladLightning <vladlen4102006@gmail.com>
2025-02-23 15:47:40 +0200

    Refactor

--- Assets/Scripts/Inheritance/MonoBehaviour/Hero/Skills/Knight/ResistanceShred.cs
+++ Assets/Scripts/Inheritance/MonoBehaviour/Hero/Skills/Knight/ResistanceShred.cs
@@ -1,34 +1,29 @@
 
-using AYellowpaper.SerializedCollections;
 using UnityEngine;
 
 public class ResistanceShred : MonoBehaviour, IPassiveHeroSkillDeactivatable
 {
     [SerializeField] private ResistanceShredSkillData _resistanceShredSkillData;
     
-    private SerializedDictionary<DamageTypesEnum.DamageTypes, float> _resistanceShredTypes;
+    private DamageTypesEnum.DamageTypes _resistanceShredType;
+    private float _resistanceShredCoefficient;
     
     private Hero _hero;
     
     private void Start()
     {
-        _resistanceShredTypes = _resistanceShredSkillData.ResistanceShredTypes;
+        _resistanceShredType = _resistanceShredSkillData.ResistanceShredType;
+        _resistanceShredCoefficient = _resistanceShredSkillData.ShredCoefficient;
         _hero = GetComponent<Hero>();
     }
 
     public void Activate()
     {
-        foreach (var damageType in _resistanceShredTypes)
-        {
-            _hero.Opponent.DecreaseDamageResistance(damageType.Value, damageType.Key);
-        }
+        _hero.Opponent.DecreaseDamageResistance(_resistanceShredCoefficient, _resistanceShredType);
     }
 
     public void Deactivate()
     {
-        foreach (var damageType in _resistanceShredTypes.Keys)
-        {
-            _hero.Opponent.ResetDamageResistance(damageType);
-        }
+        _hero.Opponent.ResetDamageResistance(_resistanceShredType);
     }
 }
--- Assets/Scripts/ScriptableObjects/Hero/Skills/ResistanceShredSkillData.cs
+++ Assets/Scripts/ScriptableObjects/Hero/Skills/ResistanceShredSkillData.cs
@@ -1,10 +1,11 @@
 
-using AYellowpaper.SerializedCollections;
 using UnityEngine;
 [CreateAssetMenu(fileName = "SkillData", menuName = "Hero/Skills/Passive/ResistanceShredSkill", order = 1)]
 public class ResistanceShredSkillData : ScriptableObject
 {
+    [SerializeField] private float _shredCoefficient;
+    public float ShredCoefficient => _shredCoefficient;
 
-    [SerializeField] private SerializedDictionary<DamageTypesEnum.DamageTypes, float> _resistanceShredTypes;
-    public SerializedDictionary<DamageTypesEnum.DamageTypes, float> ResistanceShredTypes => _resistanceShredTypes;
+    [SerializeField] private DamageTypesEnum.DamageTypes _resistanceShredType;
+    public DamageTypesEnum.DamageTypes ResistanceShredType => _resistanceShredType;
 }
