dd6db6b3415c7717262f6e480ebc516a9e4a1d57
VladLightning <vladlen4102006@gmail.com>
2025-04-30 14:09:52 +0300

    Added editor save load.

--- Assets/Scripts/UI/Saves/EditorLoad.cs
+++ Assets/Scripts/UI/Saves/EditorLoad.cs
@@ -1,9 +1,11 @@
+#if UNITY_EDITOR
 using UnityEngine;
 
-public class Load : MonoBehaviour
+public class EditorLoad : MonoBehaviour
 {
     private void Awake()
     {
         Save.LoadGame();
     }
 }
+#endif
