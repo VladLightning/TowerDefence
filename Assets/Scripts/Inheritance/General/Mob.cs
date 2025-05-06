86d0b91dd1500cc8f51b80dbcf48b6abb9580649
VladLightning <vladlen4102006@gmail.com>
2025-05-06 13:09:59 +0300

    Refactor

--- Assets/Scripts/Inheritance/General/Mob.cs
+++ Assets/Scripts/Inheritance/General/Mob.cs
@@ -33,7 +33,7 @@ public abstract class Mob : Entity, IDamageDealer
 
     private SerializedDictionary<DamageTypesEnum, float> _defaultDamageResistances;
 
-    [SerializeField] protected SerializedDictionary<DamageTypesEnum, float> _currentDamageResistances = new();
+    protected SerializedDictionary<DamageTypesEnum, float> _currentDamageResistances = new();
     
     protected HealthbarView _healthBarView;
     
